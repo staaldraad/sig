2b074f09edfe2f4f48a7b48fe05f38defb8f416c
Etienne Stalmans <etienne.stalmans@aiven.io>
2023-03-31 12:16:09 +0200

    test_secret_commit

--- test_cred.js
+++ test_cred.js
@@ -1,1 +1,1 @@
-DB_URL=rediss://default:AVNS_15ifsr1t2cEC2Hc1NrV@private-db-redis-nyc3-67768-do-user-10228058-0.b.db.ondigitalocean.com:25061/0
+DB_URL=rediss://default:AVNS_25ifsr1t2cEC2Hc1NrV@private-db-redis-nyc3-67768-do-user-10228058-0.b.db.ondigitalocean.com:25061/0
